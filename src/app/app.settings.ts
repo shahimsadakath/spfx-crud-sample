646f46a89279754423fff4e6d933647da1ab7a81
shahimsadakath <shahim.sadakath@gmail.com>
2018-06-02 15:27:50 +0300

    Update app.settings.ts

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,9 +1,12 @@
 export class AppSettings {
     
      
-    public static SP_URL = "https://shadotnet.sharepoint.com";
-    public static SP_LISTNAME = "StoreLocations";
-    public static GMAP_URL = "https://maps.google.com/maps/api/js?key=AIzaSyCj7av5uGjKw0mqcKeTCQLAaGlC1SV94e4";
+    public static SP_URL = "{YOUR_SHAREPOINT_ONLINE_URL}";
+
+    public static SP_LISTNAME = "{NAME_OF_SP_LIST}";
+
+    public static GMAP_URL = "https://maps.google.com/maps/api/js?key={YOURKEY}";
+
     public static JQUERY_URL = "https://code.jquery.com/jquery-3.3.1.min.js";
  }
  
